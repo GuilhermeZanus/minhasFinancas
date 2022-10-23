788cde89c69f3ad34be31979d65cbec5931c4291
Guilherme <guilherme.zanus@gmail.com>
2022-10-23 20:23:39 -0300

    Atualização classe UsuarioServiceTest

--- minhasFinancas/src/test/java/com/Guilherme/minhasFinancas/service/UsuarioServiceTest.java
+++ minhasFinancas/src/test/java/com/Guilherme/minhasFinancas/service/UsuarioServiceTest.java
@@ -1,5 +1,8 @@
 package com.Guilherme.minhasFinancas.service;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
 //
@@ -12,6 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.Guilherme.minhasFinancas.model.entity.Usuario;
 //
 import com.Guilherme.minhasFinancas.model.repository.UsuarioRepository;
 import com.Guilherme.minhasFinancas.service.impl.UsuarioServiceImpl;
@@ -51,6 +55,37 @@ public class UsuarioServiceTest {
 		
 		//acao
 		service.validarEmail("email@email.com");		
+	}
+	
+	@Test //(expected = Test.None.class)
+	public void deveAutenticarUmUsuarioComSucesso() {
+		
+		//cenario
+		String email = "email@email.com";
+		String senha = "senha";
+		
+		Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();		
+		
+		Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+		
+		//acao
+		Usuario result = service.autenticar(email, senha);
+		
+		//verificacao
+		Assertions.assertThat(result).isNotNull();
+		
+	}
+	
+	
+	@Test //(expected = ErroAutenticacao.class)
+	public void deveLancarErroQuandoNaoEncontrarUsuarioCadastradoComEmailInformado() {
+		
+		//cenario			
+		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+		
+		//acao
+		service.autenticar("email@email.com", "senha");
+
 	}
 	
 }
